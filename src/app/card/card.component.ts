50d984e238be9eb99cd0a24e032381aa7c4e539a
Guilherme Serra <guilhermeserrasd@gmail.com>
2025-01-31 15:40:41 -0300

    adicionando seções dos alimentos, conteúdo principal ok

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,13 @@
-import { Component, Input } from '@angular/core';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { Component, Input } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-card',
-  imports: [CommonModule],
-  templateUrl: './card.component.html',
-  styleUrl: './card.component.css',
-})
-export class CardComponent {
-  @Input() variant: 'primary' | 'secondary' = 'primary';
-}
+// @Component({
+//   selector: 'app-card',
+//   imports: [CommonModule],
+//   templateUrl: './card.component.html',
+//   styleUrl: './card.component.css',
+// })
+// export class CardComponent {
+//   @Input() variant: 'primary' | 'secondary' = 'primary';
+// }

61d2a44c71c69e31f688f1197ac502833e2ddd25
Guilherme Serra <guilhermeserrasd@gmail.com>
2025-02-03 17:41:24 -0300

    nova estilização aplicada ao footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { PlacefoodComponent } from './components/placefood/placefood.component';
 import { NavComponent } from './components/nav/nav.component';
 import { NzDemoCarouselAutoplayComponent } from './components/carousel01/carousel01.component';
 import { ContainerComponent } from './components/container/container.component';
-import { FooterComponent } from "./components/footer/footer.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -18,12 +19,13 @@ import { FooterComponent } from "./components/footer/footer.component";
     MatButtonModule,
     MatCardModule,
     MatInputModule,
+    MatIconModule,
     PlacefoodComponent,
     NavComponent,
     ContainerComponent,
     NzDemoCarouselAutoplayComponent,
-    FooterComponent
-],
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

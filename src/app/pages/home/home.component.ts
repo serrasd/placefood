070f50aa213b2c49b5fa213ecc665f81fdddf6da
Guilherme Serra <guilhermeserrasd@gmail.com>
2025-02-03 08:55:26 -0300

    salvando alterações antes de mudar de branch

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -12,22 +12,65 @@ import { CardProdutosComponent } from '../../card-produtos/card-produtos.compone
 })
 export class HomeComponent {
   produtos = [
-    { nome: 'Filé de Frango Peito Congelado sem Osso Sadia 1Kg', imagem: 'assets/file-de-frango.png', preco: 23.49 },
-    { nome: 'Picanha Ouro Congelada Swift 1,1Kg', imagem: 'assets/picanha-ouro.png', preco: 104.39 },
-    { nome: 'Linguiça Toscana Inteira Sadia 600g', imagem: 'assets/ling-toscana.png', preco: 18.47 },
-    { nome: 'Pão de alho Santa Massa Tradicional 400g', imagem: 'assets/pao-de-alho.png', preco: 16.82 },
+    {
+      nome: 'Filé de Frango Peito Congelado sem Osso Sadia 1Kg',
+      imagem: 'assets/file-de-frango.png',
+      preco: 23.49,
+    },
+    {
+      nome: 'Picanha Ouro Congelada Swift 1,1Kg',
+      imagem: 'assets/picanha-ouro.png',
+      preco: 104.39,
+    },
+    {
+      nome: 'Linguiça Toscana Inteira Sadia 600g',
+      imagem: 'assets/ling-toscana.png',
+      preco: 18.47,
+    },
+    {
+      nome: 'Pão de alho Santa Massa Tradicional 400g',
+      imagem: 'assets/pao-de-alho.png',
+      preco: 16.82,
+    },
   ];
   destaques = [
-    { nome: 'Cerveja Heineken Lata 269ml', imagem: 'assets/cerveja-heineken-lata.png', preco: 3.99 },
-    { nome: 'Suco Kapo Morango 200ml', imagem: 'assets/suco-del-valle-kapo.png', preco: 2.59 },
-    { nome: 'Café em Pó 3 Corações Tradicional 500g', imagem: 'assets/cafe-em-po-3-coracoes-tradicional.png', preco: 28.99 },
-    { nome: 'Leite Integral UHT Italac 1 Litro', imagem: 'assets/leite-integral-italac.png', preco: 4.99 },
+    {
+      nome: 'Cerveja Heineken Lata 269ml',
+      imagem: 'assets/cerveja-heineken-lata.png',
+      preco: 3.99,
+    },
+    {
+      nome: 'Suco Kapo Morango 200ml',
+      imagem: 'assets/suco-del-valle-kapo.png',
+      preco: 2.59,
+    },
+    {
+      nome: 'Café em Pó 3 Corações Tradicional 500g',
+      imagem: 'assets/cafe-em-po-3-coracoes-tradicional.png',
+      preco: 28.99,
+    },
+    {
+      nome: 'Leite Integral UHT Italac 1 Litro',
+      imagem: 'assets/leite-integral-italac.png',
+      preco: 4.99,
+    },
   ];
   hortifruti = [
     { nome: 'Maçã Gala Aprox 600g', imagem: 'assets/maca.png', preco: 8.63 },
-    { nome: 'Alface Crespa Higienizada Pacote 150g', imagem: 'assets/alface.png', preco: 6.99 },
-    { nome: 'Melancia Sugar Baby Aprox 4kg', imagem: 'assets/melancia.png', preco: 21.56 },
-    { nome: 'Limão Orgânico 500g', imagem: 'assets/limao-organico.png', preco: 6.79 },
+    {
+      nome: 'Alface Crespa Higienizada Pacote 150g',
+      imagem: 'assets/alface.png',
+      preco: 6.99,
+    },
+    {
+      nome: 'Melancia Sugar Baby Aprox 4kg',
+      imagem: 'assets/melancia.png',
+      preco: 21.56,
+    },
+    {
+      nome: 'Limão Orgânico 500g',
+      imagem: 'assets/limao-organico.png',
+      preco: 6.79,
+    },
   ];
 }
-

6644038648cf7c7902ced92b522563426321dee9
Guilherme Serra <guilhermeserrasd@gmail.com>
2025-02-10 15:41:15 -0300

    login de admin adicionado com a rota ao clicar em 'entrar'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { AdminComponent } from './pages//admin/admin.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,7 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-  }
+  },
+  { path: 'admin',
+    component: AdminComponent }
 ];
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  usuario: string = '';
+  senha: string = '';
 
+  constructor(private router: Router) {}
+
+  onSubmit(): void {
+    if (this.usuario === 'admin' && this.senha === 'admin') {
+      this.router.navigate(['/admin']);
+    } else {
+      alert('Usuário ou senha incorretos!');
+    }
+  }
 }

82bc3836485502bf207b31924ac4a0755c42a08f
Guilherme Serra <guilhermeserrasd@gmail.com>
2025-02-13 18:35:38 -0300

    tabela consumindo o json, pendente de aplicar os métodos do crud, estilização e fazer a home consumir o json

--- src/app/components/tabela/tabela.component.ts
+++ src/app/components/tabela/tabela.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Produto } from "../../interface/produto.model"
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tabela',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './tabela.component.html',
   styleUrl: './tabela.component.css'
 })
 export class TabelaComponent {
-    
+  produtos: Produto[] = [];
+
+   constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.mostrarProdutos();
+  }
+
+mostrarProdutos(): void {
+  this.http.get<any>('assets/produtos.json').subscribe((data) => {
+    this.produtos = [...data.produtos, ...data.destaques, ...data.hortifruti];
+  });
+}
+
+
 }

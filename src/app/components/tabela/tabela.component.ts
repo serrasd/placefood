1f934b268ff9417e7ae3e4e788821db12866fbb7
Guilherme Serra <guilhermeserrasd@gmail.com>
2025-02-17 13:10:09 -0300

    fix: corrigido bug do filtro na tabela que nao aplicava a paginação após uma filtragem, trazendo todos os produtos na mesma página

--- src/app/components/tabela/tabela.component.ts
+++ src/app/components/tabela/tabela.component.ts
@@ -16,69 +16,72 @@ import { FormsModule } from '@angular/forms';
 export class TabelaComponent {
   produtos: Produto[] = [];
   produtosFiltrados: Produto[] = [];
+  produtosExibidos: Produto[] = [];
   filtro: string = '';
   paginaAtual: number = 1;
   itensPorPagina: number = 10;
   totalPaginas: number = 0;
 
-   constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.mostrarProdutos();
   }
 
-mostrarProdutos(): void {
-  this.http.get<any>('assets/produtos.json').subscribe((data) => {
-    this.produtos = [...data.produtos, ...data.destaques, ...data.hortifruti];
-    this.produtosFiltrados = [...this.produtos];
-    this.totalPaginas = Math.ceil(this.produtos.length / this.itensPorPagina);
-    this.atualizarPagina();
-  });
-}
+  mostrarProdutos(): void {
+    this.http.get<any>('assets/produtos.json').subscribe((data) => {
+      this.produtos = [...data.produtos, ...data.destaques, ...data.hortifruti];
+      this.produtosFiltrados = [...this.produtos];
+      this.totalPaginas = Math.ceil(this.produtosFiltrados.length / this.itensPorPagina);
+      this.atualizarPagina();
+    });
+  }
 
-filtrarProdutos(): void {
-  const filtroLowerCase = this.filtro.toLowerCase();
-  this.produtosFiltrados = this.produtos.filter((produto) =>
-    produto.nome.toLowerCase().includes(filtroLowerCase)
-  );
-}
-
-deletar(id: string) {
-  const confirmacao = window.confirm('Você tem certeza que deseja excluir este produto?');
-  if (confirmacao) {
-    console.log('Botão clicado');
-    this.http.delete(`http://localhost:3000/deletar-produto/${id}`).subscribe(
-      (response) => {
-        console.log('Produto deletado com sucesso', response);
-      },
-      (error) => {
-        console.log('Erro ao deletar produto', error);
-      }
+  filtrarProdutos(): void {
+    const filtroLowerCase = this.filtro.toLowerCase();
+    this.produtosFiltrados = this.produtos.filter((produto) =>
+      produto.nome.toLowerCase().includes(filtroLowerCase)
     );
-  } else {
-    console.log('Exclusão cancelada');
-  }
-}
 
-atualizarPagina(): void {
-  const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
-  const fim = inicio + this.itensPorPagina;
-  this.produtosFiltrados = this.produtos.slice(inicio, fim);
-}
-
-proximaPagina(): void {
-  if (this.paginaAtual < this.totalPaginas) {
-    this.paginaAtual++;
+    this.totalPaginas = Math.ceil(this.produtosFiltrados.length / this.itensPorPagina);
+    this.paginaAtual = 1;
     this.atualizarPagina();
   }
-}
 
-paginaAnterior(): void {
-  if (this.paginaAtual > 1) {
-    this.paginaAtual--;
-    this.atualizarPagina();
+  deletar(id: string) {
+    const confirmacao = window.confirm('Você tem certeza que deseja excluir este produto?');
+    if (confirmacao) {
+      console.log('Botão clicado');
+      this.http.delete(`http://localhost:3000/deletar-produto/${id}`).subscribe(
+        (response) => {
+          console.log('Produto deletado com sucesso', response);
+        },
+        (error) => {
+          console.log('Erro ao deletar produto', error);
+        }
+      );
+    } else {
+      console.log('Exclusão cancelada');
+    }
   }
-}
 
+  atualizarPagina(): void {
+    const inicio = (this.paginaAtual - 1) * this.itensPorPagina;
+    const fim = inicio + this.itensPorPagina;
+    this.produtosExibidos = this.produtosFiltrados.slice(inicio, fim);
+  }
 
+  proximaPagina(): void {
+    if (this.paginaAtual < this.totalPaginas) {
+      this.paginaAtual++;
+      this.atualizarPagina();
+    }
+  }
+
+  paginaAnterior(): void {
+    if (this.paginaAtual > 1) {
+      this.paginaAtual--;
+      this.atualizarPagina();
+    }
+  }
 }
